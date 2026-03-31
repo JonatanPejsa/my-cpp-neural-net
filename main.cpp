b3dbcd2661e02c76f86cc9a3ef312356f73c143a
Jonatan Pejša <jonatan.pejsa.jo@gmail.com>
2026-04-01 00:06:45 +0200

    forward propagation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+#include <cmath>
+#include <functional>
 #include <iostream>
+#include <valarray>
 #include <vector>
+float relu(float input) {
+  if (input > 0.0f)
+    return input;
+  return 0.0f;
+}
+
+float sigmoid(float input) { return 1.0f / (1.0f + std::exp(-input)); }
 
 struct Matrix {
   Matrix(size_t numberOfRowsInput, size_t numberOfColumsInput)
@@ -7,7 +17,9 @@ struct Matrix {
         data(numberOfColums * numberOfRows, 0.0) {}
   void random() {
     for (size_t i = 0; i < data.size(); ++i) {
-      float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+      float r =
+          static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 2.0f -
+          1.0f;
       data[i] = r;
     }
   }
@@ -20,6 +32,32 @@ struct Matrix {
     }
     return stream;
   }
+
+  friend std::valarray<float> operator*(const Matrix &matrix,
+                                        const std::valarray<float> &vector) {
+    std::valarray<float> output(0.0, matrix.numberOfRows);
+    for (size_t i = 0; i < matrix.numberOfRows; ++i) {
+      for (size_t j = 0; j < matrix.numberOfColums; ++j) {
+        output[i] += vector[j] * matrix.data[i * matrix.numberOfColums + j];
+      }
+    }
+    return output;
+  }
+
+  friend Matrix operator*(const Matrix &matrix1, const Matrix &matrix2) {
+    Matrix output(matrix1.numberOfRows, matrix2.numberOfColums);
+    for (size_t i = 0; i < matrix1.numberOfRows; ++i) {
+      for (size_t k = 0; k < matrix2.numberOfColums; ++k) {
+        float sum = 0.0;
+        for (size_t j = 0; j < matrix2.numberOfRows; ++j) {
+          sum += matrix1.data[j + i * matrix1.numberOfColums] *
+                 matrix2.data[j * matrix2.numberOfColums + k];
+        }
+        output.data[i * matrix2.numberOfColums + k] = sum;
+      }
+    }
+    return output;
+  }
   size_t numberOfRows;
   size_t numberOfColums;
   std::vector<float> data;
@@ -27,14 +65,20 @@ struct Matrix {
 
 class Layer {
 public:
-  Layer(size_t numberOfNeuronsInput, size_t numberOfPrevNeuronsInput)
+  Layer(size_t numberOfNeuronsInput, size_t numberOfPrevNeuronsInput,
+        std::function<float(float)> activationFunctionInput)
       : numberOfNeurons(numberOfNeuronsInput),
         numNeuronsPrevLayer(numberOfPrevNeuronsInput),
-        weights(numberOfNeurons, numNeuronsPrevLayer) {
+        weights(numberOfNeurons, numNeuronsPrevLayer),
+        activationFunction(activationFunctionInput) {
+    std::vector<float> tmp;
     for (size_t i = 0; i < numberOfNeurons; ++i) {
-      float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-      bias.push_back(r);
+      float r =
+          (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f -
+          1.0f;
+      tmp.push_back(r);
     }
+    bias = std::valarray<float>(tmp.data(), tmp.size());
     weights.random();
   }
   friend std::ostream &operator<<(std::ostream &stream, const Layer &layer) {
@@ -46,23 +90,36 @@ public:
     }
     return stream;
   }
-  std::vector<float> forwardPropagation(std::vector<float> input) {
-    std::vector<float> output;
-    output.reserve(numberOfNeurons);
+  std::valarray<float> forwardPropagation(std::valarray<float> input) {
+    std::valarray<float> output;
+    output = activationFunctionArray(weights * input + bias);
     return output;
+  }
+  std::valarray<float> activationFunctionArray(std::valarray<float> array) {
+    for (float &x : array) {
+      x = activationFunction(x);
+    }
+    return array;
   }
 
 private:
   size_t numberOfNeurons;
   size_t numNeuronsPrevLayer;
   Matrix weights;
-  std::vector<float> bias;
+  std::valarray<float> bias;
+  std::function<float(float)> activationFunction;
 };
 
 int main() {
-  Layer layer(5, 8);
+  Layer layer(5, 8, relu);
 
   std::cout << "funguje" << std::endl;
   std::cout << layer << std::endl;
+
+  std::valarray<float> input(1.1f, 8);
+  std::valarray<float> output = layer.forwardPropagation(input);
+  for (auto i : output) {
+    std::cout << i << std::endl;
+  }
   return 0;
 }
